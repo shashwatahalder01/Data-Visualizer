94eef440af15e8eb5732919cb8db0071305657fb
shashwata Halder <shashwatahalder01@gmail.com>
2021-04-27 15:06:41 +0600

    Update settings.py

--- AuditDataProject/settings.py
+++ AuditDataProject/settings.py
@@ -21,8 +21,7 @@ STATIC_DIR=os.path.join(BASE_DIR, 'Static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-==ims-#m2as(k^wg4o)sg!q0z4+%!hjx6j$$a4!+j&16z%9s_7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
